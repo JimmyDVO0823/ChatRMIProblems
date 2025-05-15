661eb915d27a933e4eb6b6b0ff4352e94d169b8b
DFRANCO <jimmyvelandia@outlook.es>
2025-05-15 11:59:57 -0500

    Prototipo de mensajes pacialmente probado y funcional. tuve que hablar conmigo mismo porque faltaba otro dispositivo, pero igual funciona y la lógica del servidor hace su trabajo. momento de empezar a conectar a la GUI

--- RMIProject/src/Invocator/Invocador.java
+++ RMIProject/src/Invocator/Invocador.java
@@ -19,15 +19,26 @@ public class Invocador {
     public static void main(String[] args) {
         final String nombreServidor = "DF-Andres-William";
         try {
+            //Se da un nombre
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Escriba el nombre");
+            String nombre = sc.nextLine();
+            //Se conecta al servidor
             Registry reg = LocateRegistry.getRegistry("localhost",3232);
             IServer server = (IServer) reg.lookup(nombreServidor);
 
-            String saludo = server.darBienvenida(nombreServidor);
+            //Se da la bienvenida, es una miniprueba
+            String saludo = server.darBienvenida(nombre);
             System.out.println(saludo);
+
+            //Se crea el objeto CallBack
             ClientCallbackImpl cb = new ClientCallbackImpl();
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Escriba Y si usted es el remitente");
+
+            //Abrimos el Scanner para elegir el remitente y destinatario
+            //Scanner sc = new Scanner(System.in);
+            System.out.println("Escriba \"Y\" si usted es el remitente");
             String remitente = sc.nextLine();
+
             if (remitente.equals("Y")) test1(cb, server);
 
         } catch (Exception e) {
@@ -46,6 +57,7 @@ public class Invocador {
      */
     public static void test1(ClientCallbackImpl cb, IServer server) throws RemoteException {
         try {
+            System.out.println("TEST");
             Scanner sc = new Scanner(System.in);
             System.out.println("Ingresar nombre de usuario");
             String username = sc.nextLine();
